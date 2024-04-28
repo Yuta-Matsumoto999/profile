97976c653a6c064d978d003b3c17efc82c62bf87
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2024-04-28 15:54:57 +0900

    add Prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
-import { Inter } from "next/font/google";
-import { Box, Text } from "@chakra-ui/react";
-import { NextPage } from "next";
+import { Inter } from 'next/font/google'
+import { Box, Text } from '@chakra-ui/react'
+import { NextPage } from 'next'
 
 const Home: NextPage = (): JSX.Element => {
-  return (
-    <Box>
-      <Text>
-        Hello World.
-      </Text>
-    </Box>
-  )
+    return (
+        <Box>
+            <Text>Hello World.</Text>
+        </Box>
+    )
 }
 
 export default Home
