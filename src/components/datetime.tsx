0ce772f50e93be7b8b2be82568c64441ae6bd335
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2024-04-30 20:46:55 +0900

    add datetime format component

---
+++ src/components/datetime.tsx
@@ -0,0 +1,30 @@
+import { Text } from '@chakra-ui/react'
+
+interface Props {
+    dateTimeString: string
+    mt: number
+    mb: number
+    fontSize: number
+    fontWeight: number
+}
+
+const Datetime = ({ dateTimeString, mt, mb, fontSize, fontWeight }: Props) => {
+    console.log(dateTimeString)
+    const date = new Date(dateTimeString)
+    const formattedDate = date.toLocaleString('ja-JP', {
+        timeZone: 'Asia/Tokyo',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+    })
+
+    return (
+        <Text mt={mt} mb={mb} fontSize={fontSize} fontWeight={fontWeight}>
+            {formattedDate}
+        </Text>
+    )
+}
+
+export default Datetime
